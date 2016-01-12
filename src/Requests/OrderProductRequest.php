1ef61f890a9e56cf9538620b33fccc9badefbe20
Tom Kay <oridan82@gmail.com>
2016-01-12 15:56:29 +0000

    Updated Auto Generated Code

--- src/Requests/OrderProductRequest.php
+++ src/Requests/OrderProductRequest.php
@@ -13,6 +13,10 @@ class OrderProductRequest
   {
     return [
       "fid" => $this->getFid(),
+      "displayName" => $this->getDisplayName(),
+      "description" => $this->getDescription(),
+      "price" => $this->getPrice(),
+      "offerFid" => $this->getOfferFid(),
     ];
   }
 
@@ -24,5 +28,45 @@ class OrderProductRequest
   public function getFid($default = null)
   {
     return Objects::property($this->_getResultJson(), 'fid', $default);
+  }
+
+  /**
+   * @param mixed $default
+   *
+   * @return string
+   */
+  public function getDisplayName($default = null)
+  {
+    return Objects::property($this->_getResultJson(), 'displayName', $default);
+  }
+
+  /**
+   * @param mixed $default
+   *
+   * @return string
+   */
+  public function getDescription($default = null)
+  {
+    return Objects::property($this->_getResultJson(), 'description', $default);
+  }
+
+  /**
+   * @param mixed $default
+   *
+   * @return integer
+   */
+  public function getPrice($default = null)
+  {
+    return Objects::property($this->_getResultJson(), 'price', $default);
+  }
+
+  /**
+   * @param mixed $default
+   *
+   * @return string
+   */
+  public function getOfferFid($default = null)
+  {
+    return Objects::property($this->_getResultJson(), 'offerFid', $default);
   }
 }
