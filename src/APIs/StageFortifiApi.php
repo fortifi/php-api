335847ae2dda58f6a725db4cfc5e1e94026e93e9
bajb <brooke@bajb.net>
2016-01-15 17:04:09 +0000

    Stage API

---
+++ src/APIs/StageFortifiApi.php
@@ -0,0 +1,19 @@
+<?php
+namespace Fortifi\Api\V1\APIs;
+
+use Fortifi\Api\Core\ApiDefinition\ApiDefinition;
+use Fortifi\Api\V1\Endpoints\FortifiApi;
+
+class StageFortifiApi extends FortifiApi
+{
+  public function __construct()
+  {
+    parent::__construct();
+    $def = $this->getApiDefinition();
+    if($def instanceof ApiDefinition)
+    {
+      $def->setHost('sapi.fortifi.io');
+    }
+  }
+
+}
