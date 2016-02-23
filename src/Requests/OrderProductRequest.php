243d602296418cabd8822792c3e2cdb49a4a0959
Tom Kay <oridan82@gmail.com>
2016-02-23 12:22:53 +0000

    Updated Auto Generated Code

--- src/Requests/OrderProductRequest.php
+++ src/Requests/OrderProductRequest.php
@@ -15,6 +15,10 @@ class OrderProductRequest
       [
         "price" => $this->getPrice(),
         "offerFid" => $this->getOfferFid(),
+        "firstChargeAmount" => $this->getFirstChargeAmount(),
+        "firstChargeDiscount" => $this->getFirstChargeDiscount(),
+        "firstChargeTax" => $this->getFirstChargeTax(),
+        "renewalDate" => $this->getRenewalDate(),
       ]
     );
   }
@@ -38,6 +42,50 @@ class OrderProductRequest
   public function getOfferFid($default = null, $trim = true)
   {
     $value = Objects::property($this->_getResultJson(), 'offerFid', $default);
+    return $trim ? trim($value) : $value;
+  }
+
+  /**
+   * @param mixed $default
+   *
+   * @return integer
+   */
+  public function getFirstChargeAmount($default = null)
+  {
+    return Objects::property($this->_getResultJson(), 'firstChargeAmount', $default);
+  }
+
+  /**
+   * @param mixed $default
+   *
+   * @return integer
+   */
+  public function getFirstChargeDiscount($default = null)
+  {
+    return Objects::property($this->_getResultJson(), 'firstChargeDiscount', $default);
+  }
+
+  /**
+   * @param mixed $default
+   *
+   * @return integer
+   */
+  public function getFirstChargeTax($default = null)
+  {
+    return Objects::property($this->_getResultJson(), 'firstChargeTax', $default);
+  }
+
+  /**
+   * Time in ISO 8601 standard with optional fractions of a second e.g 2015-12-05T13:11:59.888Z
+   * 
+   * @param mixed $default
+   * @param bool $trim Trim Value
+   *
+   * @return string
+   */
+  public function getRenewalDate($default = null, $trim = true)
+  {
+    $value = Objects::property($this->_getResultJson(), 'renewalDate', $default);
     return $trim ? trim($value) : $value;
   }
 }
