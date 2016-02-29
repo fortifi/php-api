d8ed4a67ed50f6d39cc0bf997e24bffd324b5adc
Tom Kay <oridan82@gmail.com>
2016-02-29 11:30:46 +0000

    Updated Auto Generated Code

---
+++ src/Enums/PaymentServiceProcessor.php
@@ -0,0 +1,30 @@
+<?php
+namespace Fortifi\Api\V1\Enums;
+
+final class PaymentServiceProcessor
+{
+  const PAYPAL = 'paypal';
+  const WIRE = 'wire';
+  const BLACKHOLE = 'blackhole';
+  const CHEQUE = 'cheque';
+  const PREPAYMENT = 'prepayment';
+  const FORTIFIPAY = 'fortifipay';
+
+  public static function getDisplayValue($value)
+  {
+    return ucfirst($value);
+  }
+
+  public static function getValues()
+  {
+    return [
+      'paypal',
+      'wire',
+      'blackhole',
+      'cheque',
+      'prepayment',
+      'fortifipay',
+    ];
+  }
+}
+      
