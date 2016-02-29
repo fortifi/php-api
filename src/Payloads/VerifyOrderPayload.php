f5698b39ffb6d9f3c77957b0cd1c737d6f477a8c
Tom Kay <oridan82@gmail.com>
2016-02-29 13:29:03 +0000

    Updated Auto Generated Code

--- src/Payloads/VerifyOrderPayload.php
+++ src/Payloads/VerifyOrderPayload.php
@@ -5,13 +5,17 @@ class VerifyOrderPayload
   implements \JsonSerializable
 {
   /**
+   * FID for the payment service you wish to charge the customer through
+   */
+  protected $_paymentServiceFid;
+  /**
    * FID for the payment account you wish to charge the customer through
    */
   protected $_paymentAccountFid;
   /**
-   * FID for the payment service you wish to charge the customer through
+   * Payment Service Processor Type
    */
-  protected $_paymentServiceFid;
+  protected $_paymentServiceProcessor;
   protected $_successUrl;
   protected $_cancelUrl;
   protected $_failUrl;
@@ -19,13 +23,17 @@ class VerifyOrderPayload
   public function hydrate($data)
   {
     $data = (array)$data;
+    if(isset($data["paymentServiceFid"]))
+    {
+      $this->_paymentServiceFid = $data["paymentServiceFid"];
+    }
     if(isset($data["paymentAccountFid"]))
     {
       $this->_paymentAccountFid = $data["paymentAccountFid"];
     }
-    if(isset($data["paymentServiceFid"]))
+    if(isset($data["paymentServiceProcessor"]))
     {
-      $this->_paymentServiceFid = $data["paymentServiceFid"];
+      $this->_paymentServiceProcessor = $data["paymentServiceProcessor"];
     }
     if(isset($data["successUrl"]))
     {
@@ -45,12 +53,38 @@ class VerifyOrderPayload
   public function jsonSerialize()
   {
     return [
-      "paymentAccountFid" => $this->_paymentAccountFid,
-      "paymentServiceFid" => $this->_paymentServiceFid,
-      "successUrl"        => $this->_successUrl,
-      "cancelUrl"         => $this->_cancelUrl,
-      "failUrl"           => $this->_failUrl,
+      "paymentServiceFid"       => $this->_paymentServiceFid,
+      "paymentAccountFid"       => $this->_paymentAccountFid,
+      "paymentServiceProcessor" => $this->_paymentServiceProcessor,
+      "successUrl"              => $this->_successUrl,
+      "cancelUrl"               => $this->_cancelUrl,
+      "failUrl"                 => $this->_failUrl,
     ];
+  }
+
+  /**
+   * @param string $value
+   *
+   * @return $this
+   */
+  public function setPaymentServiceFid($value)
+  {
+    $this->_paymentServiceFid = $value;
+    return $this;
+  }
+
+  /**
+   * FID for the payment service you wish to charge the customer through
+   *
+   * @param mixed $default
+   * @param bool $trim Trim Value
+   *
+   * @return string
+   */
+  public function getPaymentServiceFid($default = null, $trim = true)
+  {
+    $value = $this->_paymentServiceFid ?: $default;
+    return $trim ? trim($value) : $value;
   }
 
   /**
@@ -83,23 +117,23 @@ class VerifyOrderPayload
    *
    * @return $this
    */
-  public function setPaymentServiceFid($value)
+  public function setPaymentServiceProcessor($value)
   {
-    $this->_paymentServiceFid = $value;
+    $this->_paymentServiceProcessor = $value;
     return $this;
   }
 
   /**
-   * FID for the payment service you wish to charge the customer through
+   * Payment Service Processor Type
    *
    * @param mixed $default
    * @param bool $trim Trim Value
    *
    * @return string
    */
-  public function getPaymentServiceFid($default = null, $trim = true)
+  public function getPaymentServiceProcessor($default = null, $trim = true)
   {
-    $value = $this->_paymentServiceFid ?: $default;
+    $value = $this->_paymentServiceProcessor ?: $default;
     return $trim ? trim($value) : $value;
   }
 
