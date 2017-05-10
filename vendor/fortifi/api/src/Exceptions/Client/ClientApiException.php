3c056a32d8cfdfb5b1be64c6199a494e270e7339
Grant Fribbens <grant.fribbens@fortifi.io>
2017-05-10 18:27:22 +0100

    Ref: T2795 - Changes from review

--- vendor/fortifi/api/src/Exceptions/Client/ClientApiException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Fortifi\Api\Core\Exceptions\Client;
-
-use Fortifi\Api\Core\Exceptions\ApiException;
-
-class ClientApiException extends ApiException
-{
-
-}
--- vendor/fortifi/api/src/Exceptions/Server/ServerApiException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Fortifi\Api\Core\Exceptions\Server;
-
-use Fortifi\Api\Core\Exceptions\ApiException;
-
-class ServerApiException extends ApiException
-{
-
-}
