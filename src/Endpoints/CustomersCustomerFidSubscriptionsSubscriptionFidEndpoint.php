30e17d85536b9b98a010a1a756bf44285240622b
richard.gooding <r@jdi.io>
2017-06-30 12:10:21 +0100

    Ref T2984 - Added call to apply offer to subscription

--- src/Endpoints/CustomersCustomerFidSubscriptionsSubscriptionFidEndpoint.php
+++ src/Endpoints/CustomersCustomerFidSubscriptionsSubscriptionFidEndpoint.php
@@ -4,6 +4,7 @@ namespace Fortifi\Api\V1\Endpoints;
 use Fortifi\Api\V1\Requests\SubscriptionRequest;
 use Fortifi\Api\V1\Requests\FidRequest;
 use Fortifi\Api\V1\Payloads\ModifySubscriptionPayload;
+use Fortifi\Api\V1\Payloads\ApplyOfferSubscriptionPayload;
 use Fortifi\Api\Core\ApiRequestDetail;
 use Fortifi\Api\Core\ApiRequest;
 use Fortifi\Api\Core\ApiEndpoint;
@@ -175,6 +176,34 @@ class CustomersCustomerFidSubscriptionsSubscriptionFidEndpoint extends ApiEndpoi
       )
     ));
     $detail->addPostField('priceFid', $priceFid);
+    $detail->setMethod('PUT');
+    $request->setRequestDetail($detail);
+    return $request;
+  }
+
+  /**
+   * @summary Apply an offer to a subscription
+   *
+   * @param ApplyOfferSubscriptionPayload $payload
+   *
+   * @return ApiRequest
+   */
+  public function setApplyOffer(ApplyOfferSubscriptionPayload $payload)
+  {
+    $request = new ApiRequest();
+    $request->setConnection($this->_getConnection());
+    $request->setEndpoint($this);
+
+    $detail = new ApiRequestDetail();
+    $detail->setRequireAuth(true);
+    $detail->setUrl($this->_buildUrl(
+      str_replace(
+        array_keys($this->_replacements),
+        array_values($this->_replacements),
+        'customers/{customerFid}/subscriptions/{subscriptionFid}/applyOffer'
+      )
+    ));
+    $detail->setBody(json_encode($payload));
     $detail->setMethod('PUT');
     $request->setRequestDetail($detail);
     return $request;
