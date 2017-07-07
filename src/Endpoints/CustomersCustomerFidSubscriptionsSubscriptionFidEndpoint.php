2c8149dfaefd9b4facd234f6e23991cae13a3c5f
rgooding <rgooding@users.noreply.github.com>
2017-07-07 11:19:53 +0100

    Updated Auto Generated Code (#4)

--- src/Endpoints/CustomersCustomerFidSubscriptionsSubscriptionFidEndpoint.php
+++ src/Endpoints/CustomersCustomerFidSubscriptionsSubscriptionFidEndpoint.php
@@ -4,7 +4,6 @@ namespace Fortifi\Api\V1\Endpoints;
 use Fortifi\Api\V1\Requests\SubscriptionRequest;
 use Fortifi\Api\V1\Requests\FidRequest;
 use Fortifi\Api\V1\Payloads\ModifySubscriptionPayload;
-use Fortifi\Api\V1\Payloads\ApplyOfferSubscriptionPayload;
 use Fortifi\Api\Core\ApiRequestDetail;
 use Fortifi\Api\Core\ApiRequest;
 use Fortifi\Api\Core\ApiEndpoint;
@@ -176,34 +175,6 @@ class CustomersCustomerFidSubscriptionsSubscriptionFidEndpoint extends ApiEndpoi
       )
     ));
     $detail->addPostField('priceFid', $priceFid);
-    $detail->setMethod('PUT');
-    $request->setRequestDetail($detail);
-    return $request;
-  }
-
-  /**
-   * @summary Apply an offer to a subscription
-   *
-   * @param ApplyOfferSubscriptionPayload $payload
-   *
-   * @return ApiRequest
-   */
-  public function setApplyOffer(ApplyOfferSubscriptionPayload $payload)
-  {
-    $request = new ApiRequest();
-    $request->setConnection($this->_getConnection());
-    $request->setEndpoint($this);
-
-    $detail = new ApiRequestDetail();
-    $detail->setRequireAuth(true);
-    $detail->setUrl($this->_buildUrl(
-      str_replace(
-        array_keys($this->_replacements),
-        array_values($this->_replacements),
-        'customers/{customerFid}/subscriptions/{subscriptionFid}/applyOffer'
-      )
-    ));
-    $detail->setBody(json_encode($payload));
     $detail->setMethod('PUT');
     $request->setRequestDetail($detail);
     return $request;
