e2e4bd0f62a7c9446e0f1c7cc5e687f57ab21fd8
Grant Fribbens <grant.fribbens@fortifi.io>
2017-08-02 09:31:34 +0100

    Ref: T2940 - Update Customer Subscription Update Renewal Price

--- src/Endpoints/CustomersCustomerFidInvoicesInvoiceFidEndpoint.php
+++ src/Endpoints/CustomersCustomerFidInvoicesInvoiceFidEndpoint.php
@@ -2,9 +2,7 @@
 namespace Fortifi\Api\V1\Endpoints;
 
 use Fortifi\Api\V1\Requests\InvoiceRequest;
-use Fortifi\Api\V1\Payloads\InvoiceCreditNotePayload;
 use Fortifi\Api\Core\ApiRequestDetail;
-use Fortifi\Api\Core\ApiRequest;
 use Fortifi\Api\Core\ApiEndpoint;
 
 class CustomersCustomerFidInvoicesInvoiceFidEndpoint extends ApiEndpoint
@@ -39,34 +37,6 @@ class CustomersCustomerFidInvoicesInvoiceFidEndpoint extends ApiEndpoint
       )
     ));
     $detail->setMethod('GET');
-    $request->setRequestDetail($detail);
-    return $request;
-  }
-
-  /**
-   * @summary Add a credit note to a customers invoice
-   *
-   * @param InvoiceCreditNotePayload $payload
-   *
-   * @return ApiRequest
-   */
-  public function createCreditNote(InvoiceCreditNotePayload $payload)
-  {
-    $request = new ApiRequest();
-    $request->setConnection($this->_getConnection());
-    $request->setEndpoint($this);
-
-    $detail = new ApiRequestDetail();
-    $detail->setRequireAuth(true);
-    $detail->setUrl($this->_buildUrl(
-      str_replace(
-        array_keys($this->_replacements),
-        array_values($this->_replacements),
-        'customers/{customerFid}/invoices/{invoiceFid}/creditNote'
-      )
-    ));
-    $detail->setBody(json_encode($payload));
-    $detail->setMethod('POST');
     $request->setRequestDetail($detail);
     return $request;
   }
