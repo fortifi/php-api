14c28c7ad6dafe42cbe8423f7aabb64fa8c4d879
Tom Kay <oridan82@gmail.com>
2018-04-26 17:27:14 +0100

    Updated Auto Generated Code

---
+++ src/Requests/ResponseRequest.php
@@ -0,0 +1,61 @@
+<?php
+namespace Fortifi\Api\V1\Requests;
+
+use Fortifi\Api\Core\ApiRequest;
+use Packaged\Helpers\Objects;
+use Packaged\Helpers\Strings;
+
+class ResponseRequest
+  extends ApiRequest
+  implements \JsonSerializable
+{
+
+  public function jsonSerialize()
+  {
+    return [
+      "requestId" => $this->getRequestId(),
+      "code" => $this->getCode(),
+      "message" => $this->getMessage(),
+    ];
+  }
+
+  /**
+   * Request ID
+   * 
+   * @param mixed $default
+   * @param bool $trim Trim Value
+   *
+   * @return string
+   */
+  public function getRequestId($default = null, $trim = true)
+  {
+    $value = Objects::property($this->_getResultJson(), 'requestId', $default);
+    return $trim ? Strings::ntrim($value) : $value;
+  }
+
+  /**
+   * Status code
+   * 
+   * @param mixed $default
+   *
+   * @return integer
+   */
+  public function getCode($default = null)
+  {
+    return Objects::property($this->_getResultJson(), 'code', $default);
+  }
+
+  /**
+   * Status message
+   * 
+   * @param mixed $default
+   * @param bool $trim Trim Value
+   *
+   * @return string
+   */
+  public function getMessage($default = null, $trim = true)
+  {
+    $value = Objects::property($this->_getResultJson(), 'message', $default);
+    return $trim ? Strings::ntrim($value) : $value;
+  }
+}
