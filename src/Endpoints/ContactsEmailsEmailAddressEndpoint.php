cfe5713b01fffd65c8270fcc2032817badea51a1
Tom Kay <oridan82@gmail.com>
2018-05-16 13:30:20 +0100

    Updated Auto Generated Code

--- src/Endpoints/ContactsEmailsEmailAddressEndpoint.php
+++ src/Endpoints/ContactsEmailsEmailAddressEndpoint.php
@@ -44,10 +44,11 @@ class ContactsEmailsEmailAddressEndpoint extends ApiEndpoint
    * @summary Unsubscribe an email address
    *
    * @param $brandFid
+   * @param $groupFid
    *
    * @return ApiRequest
    */
-  public function setUnsubscribe($brandFid = null)
+  public function setUnsubscribe($brandFid = null, $groupFid = null)
   {
     $request = new ApiRequest();
     $request->setConnection($this->_getConnection());
@@ -63,6 +64,49 @@ class ContactsEmailsEmailAddressEndpoint extends ApiEndpoint
       )
     ));
     $detail->addPostField('brandFid', $brandFid);
+    $detail->addPostField('groupFid', $groupFid);
+    $detail->setMethod('PUT');
+    $request->setRequestDetail($detail);
+    return $request;
+  }
+
+  /**
+   * @summary Subscribe an email address
+   *
+   * @param $brandFid
+   * @param $groupFid
+   * @param $userAgent
+   * @param $encoding
+   * @param $language
+   * @param $clientIp
+   * @param $optInStatus
+   * @param $optInData
+   *
+   * @return ApiRequest
+   */
+  public function setSubscribe($brandFid = null, $groupFid = null, $userAgent = null, $encoding = null, $language = null, $clientIp = null, $optInStatus = null, $optInData = null)
+  {
+    $request = new ApiRequest();
+    $request->setConnection($this->_getConnection());
+    $request->setEndpoint($this);
+
+    $detail = new ApiRequestDetail();
+    $detail->setRequireAuth(true);
+    $detail->setUrl($this->_buildUrl(
+      str_replace(
+        array_keys($this->_replacements),
+        array_values($this->_replacements),
+        'contacts/emails/{emailAddress}/subscribe'
+      )
+    ));
+    $detail->addPostField('brandFid', $brandFid);
+    $detail->addPostField('groupFid', $groupFid);
+    $detail->addPostField('userAgent', $userAgent);
+    $detail->addPostField('encoding', $encoding);
+    $detail->addPostField('language', $language);
+    $detail->addPostField('clientIp', $clientIp);
+    $detail->addPostField('optInStatus', $optInStatus);
+    $detail->addPostField('optInData', $optInData);
     $detail->setMethod('PUT');
     $request->setRequestDetail($detail);
     return $request;
