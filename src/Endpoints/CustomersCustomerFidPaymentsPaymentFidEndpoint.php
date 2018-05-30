f970123e5dee23be7611a32470952c1232586ed0
Steve Barrow <steve@infurio.com>
2018-05-30 10:06:19 +0100

    Updated Auto Generated Code

--- src/Endpoints/CustomersCustomerFidPaymentsPaymentFidEndpoint.php
+++ src/Endpoints/CustomersCustomerFidPaymentsPaymentFidEndpoint.php
@@ -1,6 +1,8 @@
 <?php
 namespace Fortifi\Api\V1\Endpoints;
 
+use Fortifi\Api\V1\Requests\PaymentRequest;
+use Fortifi\Api\Core\ApiRequestDetail;
 use Fortifi\Api\Core\ApiEndpoint;
 
 class CustomersCustomerFidPaymentsPaymentFidEndpoint extends ApiEndpoint
@@ -25,5 +27,30 @@ class CustomersCustomerFidPaymentsPaymentFidEndpoint extends ApiEndpoint
     );
     $endpoint->_buildFromEndpoint($this);
     return $endpoint;
+  }
+
+  /**
+   * @summary Retrieve a payment
+   *
+   * @return PaymentRequest
+   */
+  public function retrieve()
+  {
+    $request = new PaymentRequest();
+    $request->setConnection($this->_getConnection());
+    $request->setEndpoint($this);
+
+    $detail = new ApiRequestDetail();
+    $detail->setRequireAuth(true);
+    $detail->setUrl($this->_buildUrl(
+      str_replace(
+        array_keys($this->_replacements),
+        array_values($this->_replacements),
+        'customers/{customerFid}/payments/{paymentFid}'
+      )
+    ));
+    $detail->setMethod('GET');
+    $request->setRequestDetail($detail);
+    return $request;
   }
 }
