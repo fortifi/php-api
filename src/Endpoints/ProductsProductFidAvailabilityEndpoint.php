9ce320e6e1cf8a18f891b053c2a035848bcba8b3
Tom Kay <oridan82@gmail.com>
2020-04-03 20:30:09 +0100

    Updated Auto Generated Code

--- src/Endpoints/ProductsProductFidAvailabilityEndpoint.php
+++ src/Endpoints/ProductsProductFidAvailabilityEndpoint.php
@@ -1,7 +1,9 @@
 <?php
 namespace Fortifi\Api\V1\Endpoints;
 
+use Fortifi\Api\V1\Requests\AvailabilityReserveRequest;
 use Fortifi\Api\V1\Requests\AvailabilityCheckRequest;
+use Fortifi\Api\V1\Payloads\AvailabilityReservePayload;
 use Fortifi\Api\V1\Payloads\AvailabilityCheckPayload;
 use Fortifi\Api\Core\ApiRequestDetail;
 use Fortifi\Api\Core\ApiEndpoint;
@@ -36,6 +38,34 @@ class ProductsProductFidAvailabilityEndpoint extends ApiEndpoint
         array_keys($this->_replacements),
         array_values($this->_replacements),
         'products/{productFid}/availability/check'
+      )
+    ));
+    $detail->setBody(json_encode($payload));
+    $detail->setMethod('POST');
+    $request->setRequestDetail($detail);
+    return $request;
+  }
+
+  /**
+   * @summary Reserve Product
+   *
+   * @param AvailabilityReservePayload $payload
+   *
+   * @return AvailabilityReserveRequest
+   */
+  public function createReserve(AvailabilityReservePayload $payload)
+  {
+    $request = new AvailabilityReserveRequest();
+    $request->setConnection($this->_getConnection());
+    $request->setEndpoint($this);
+
+    $detail = new ApiRequestDetail();
+    $detail->setRequireAuth(true);
+    $detail->setUrl($this->_buildUrl(
+      str_replace(
+        array_keys($this->_replacements),
+        array_values($this->_replacements),
+        'products/{productFid}/availability/reserve'
       )
     ));
     $detail->setBody(json_encode($payload));
