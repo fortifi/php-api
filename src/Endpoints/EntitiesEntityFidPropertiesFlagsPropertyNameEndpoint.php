8c47021925db8fd69237b1dacaf91c802b5fc558
Tom Kay <oridan82@gmail.com>
2020-05-19 15:29:30 +0100

    Updated Auto Generated Code

--- src/Endpoints/EntitiesEntityFidPropertiesFlagsPropertyNameEndpoint.php
+++ src/Endpoints/EntitiesEntityFidPropertiesFlagsPropertyNameEndpoint.php
@@ -1,6 +1,7 @@
 <?php
 namespace Fortifi\Api\V1\Endpoints;
 
+use Fortifi\Api\V1\Requests\PropertyFlagRequest;
 use Fortifi\Api\V1\Payloads\PropertyFlagPayload;
 use Fortifi\Api\Core\ApiRequestDetail;
 use Fortifi\Api\Core\ApiRequest;
@@ -15,6 +16,31 @@ class EntitiesEntityFidPropertiesFlagsPropertyNameEndpoint extends ApiEndpoint
   {
     $this->_replacements['{entityFid}'] = $entityFid;
     $this->_replacements['{propertyName}'] = $propertyName;
+  }
+
+  /**
+   * @summary Get a flag property from an entity
+   *
+   * @return PropertyFlagRequest
+   */
+  public function retrieve()
+  {
+    $request = new PropertyFlagRequest();
+    $request->setConnection($this->_getConnection());
+    $request->setEndpoint($this);
+
+    $detail = new ApiRequestDetail();
+    $detail->setRequireAuth(true);
+    $detail->setUrl($this->_buildUrl(
+      str_replace(
+        array_keys($this->_replacements),
+        array_values($this->_replacements),
+        'entities/{entityFid}/properties/flags/{propertyName}'
+      )
+    ));
+    $detail->setMethod('GET');
+    $request->setRequestDetail($detail);
+    return $request;
   }
 
   /**
