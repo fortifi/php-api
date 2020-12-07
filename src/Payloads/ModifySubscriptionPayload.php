02ee297dd6f8da50f0722d01bfab7f3439a7ac3f
Brooke Bryan <brooke@bajb.net>
2020-12-07 15:19:09 +0000

    Updated Auto Generated Code

--- src/Payloads/ModifySubscriptionPayload.php
+++ src/Payloads/ModifySubscriptionPayload.php
@@ -11,9 +11,13 @@ class ModifySubscriptionPayload
    */
   protected $_priceFid;
   /**
-   * Price FID to modify subscription with
+   * Offer FID to apply to the modify order item
    */
   protected $_offerFid;
+  /**
+   * SKU or SkuFid to modify subscription with
+   */
+  protected $_sku;
   /**
    * Mode to modify subscription with
    */
@@ -30,6 +34,10 @@ class ModifySubscriptionPayload
     {
       $this->_offerFid = (string)$data["offerFid"];
     }
+    if(isset($data["sku"]))
+    {
+      $this->_sku = (string)$data["sku"];
+    }
     if(isset($data["mode"]))
     {
       $this->_mode = (string)$data["mode"];
@@ -42,6 +50,7 @@ class ModifySubscriptionPayload
     return [
       "priceFid" => $this->_priceFid,
       "offerFid" => $this->_offerFid,
+      "sku"      => $this->_sku,
       "mode"     => $this->_mode,
     ];
   }
@@ -83,7 +92,7 @@ class ModifySubscriptionPayload
   }
 
   /**
-   * Price FID to modify subscription with
+   * Offer FID to apply to the modify order item
    *
    * @param mixed $default
    * @param bool $trim Trim Value
@@ -93,6 +102,31 @@ class ModifySubscriptionPayload
   public function getOfferFid($default = null, $trim = true)
   {
     $value = $this->_offerFid ?: $default;
+    return $trim ? Strings::ntrim($value) : $value;
+  }
+
+  /**
+   * @param string $value
+   *
+   * @return $this
+   */
+  public function setSku(?string $value)
+  {
+    $this->_sku = $value;
+    return $this;
+  }
+
+  /**
+   * SKU or SkuFid to modify subscription with
+   *
+   * @param mixed $default
+   * @param bool $trim Trim Value
+   *
+   * @return string
+   */
+  public function getSku($default = null, $trim = true)
+  {
+    $value = $this->_sku ?: $default;
     return $trim ? Strings::ntrim($value) : $value;
   }
 
